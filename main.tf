3af790c1f684928424f3c32fe8b01ab70accf0c6
swayamoza404 <swayamoza404@gmail.com>
2025-02-08 15:46:44 +0530

    main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = "~> 1.1"
+  required_providers {
+    aws = {
+      version = "~>3.1"
+    }
+  }
+}
+provider "aws" {
+  region = var.my_region
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+resource "aws_instance" "myec2" {
+ 
+  ami           = var.my_ami
+  instance_type = t2.micro
+  tags = {
+    Name = "myinstance "
+  }
+  key_name = "mytf-key"
+provisioner "local-exec" {
+   command = "echo ${aws_instance.myec2.public_ip} > ip.txt"
+}
+variable "my_region{
+type = string
+deafult = "us-east-1"
+}
+variable "access_key" {}
+variable "secret_key" {}
+variable "my_ami" {
+deafult = "ami-04b4f1a9cf54c11d0"
+}
