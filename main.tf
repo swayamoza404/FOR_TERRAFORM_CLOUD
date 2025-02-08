7c404ffe874d85553f1c9aaa155d70b3091a7f37
swayamoza404 <swayamoza404@gmail.com>
2025-02-08 16:05:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,22 +12,19 @@ provider "aws" {
   secret_key = var.secret_key
 }
 resource "aws_instance" "myec2" {
- 
   ami           = var.my_ami
-  instance_type = t2.micro
+  instance_type = "t2.micro"
   tags = {
     Name = "myinstance "
   }
-  key_name = "mytf-key"
-provisioner "local-exec" {
-   command = "echo ${aws_instance.myec2.public_ip} > ip.txt"
 }
-variable "my_region{
-type = string
-deafult = "us-east-1"
+variable "my_region" {
+   type = string
+  default = "ap-south-1"
 }
-variable "access_key" {}
-variable "secret_key" {}
+variable "access_key" { }
+variable "secret_key" { }
 variable "my_ami" {
-deafult = "ami-04b4f1a9cf54c11d0"
+   type = string
+   default = "ami-0c50b6f7dc3701ddd"
 }
